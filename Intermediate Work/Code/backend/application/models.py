0fbde29a068db7631433b761e99e6e619daf8094
Chirag-Goel-17 <21f2000540@ds.study.iitm.ac.in>
2023-03-07 10:07:31 +0530

    login function and authenticaion

--- Intermediate Work/Code/backend/application/models.py
+++ Intermediate Work/Code/backend/application/models.py
@@ -1,14 +1,15 @@
-from flask import *
+from sqlalchemy.ext.declarative import declarative_base
 from flask_sqlalchemy import SQLAlchemy
+import functools
+from flask import request, jsonify
 import jwt
-from functools import wraps
-from application import app
 
+engine = None
+Base = declarative_base()
 db = SQLAlchemy()
-db.init_app(app)
-app.app_context().push()
 
 class User(db.Model):
+    # __tablename__ = 'user'
     user_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
     user_name=db.Column(db.String(100),unique=True,nullable=False)
     name=db.Column(db.String(100),nullable=False)
@@ -17,20 +18,20 @@ class User(db.Model):
     role_id=db.Column(db.Integer,nullable=False)
     
 class Student(db.Model):
-    user_id=db.Column(db.Integer, db.ForeignKey('User.user_id'),primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.user_id'),primary_key=True)
     roll_number=db.Column(db.String(100),nullable=False,primary_key=True)
     
-    
 class Support_Agent(db.Model):
-    user_id=db.Column(db.Integer, db.ForeignKey('User.user_id'),primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.user_id'),primary_key=True)
     agent_id=db.Column(db.String(100),nullable=False,primary_key=True)
+
     
 class Admin(db.Model):
-    user_id=db.Column(db.Integer, db.ForeignKey('User.user_id'),primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.user_id'),primary_key=True)
     admin_id=db.Column(db.String(100),nullable=False,primary_key=True)
     
 class Manager(db.Model):
-    user_id=db.Column(db.Integer, db.ForeignKey('User.user_id'),primary_key=True)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.user_id'),primary_key=True)
     manager_id=db.Column(db.String(100),nullable=False,primary_key=True)
 
 class Ticket(db.Model):
@@ -38,32 +39,30 @@ class Ticket(db.Model):
     title=db.Column(db.String(100),nullable=False)
     description=db.Column(db.String(100),nullable=False)
     creation_date=db.Column(db.DateTime,nullable=False)
-    creator_id=db.Column(db.Integer, db.ForeignKey('User.user_id'))
-    responder_id=db.Column(db.Integer, db.ForeignKey('User.user_id'))
+    creator_id=db.Column(db.Integer, db.ForeignKey('user.user_id'))
+    responder_id=db.Column(db.Integer, db.ForeignKey('user.user_id'))
     number_of_upvotes=db.Column(db.Integer,default=0)
     is_read=db.Column(db.Boolean,nullable=False)
     is_open=db.Column(db.Boolean,nullable=False)
     is_offensive=db.Column(db.Boolean,nullable=False)
     is_FAQ=db.Column(db.Boolean,nullable=False)
     
-# decorator for verifying the JWT
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        token = None
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-        if not token:
-            return jsonify({'message': 'Token is missing !!'}), 401
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=["HS256"])
-            current_user = User.query.filter_by(public_id=data['public_id']).first()
-        except:
-            return jsonify({
-                'message': 'Token is invalid !!'
-            }), 401
-        return f(current_user, *args, **kwargs)
-
-    return decorated
-    
-    
+def authtoken_required(function):
+	@functools.wraps(function)
+	def loggedin(*args,**kwargs):
+		auth_token=None
+		try:
+			auth_token = request.headers['secret_authtoken']
+		
+		except:
+			return jsonify({"status":'unsuccessful, missing the authtoken'})
+		
+		try: 
+			output = jwt.decode(auth_token,app.config['SECRET_KEY'])
+			#print(output)
+			user = User.query.filter_by(user_id = output["user_id"]).first()
+		except:
+			return jsonify({"status":"failure, your token details do not match"})
+		
+		return function(*args,user.user_id,**kwargs)
+	return loggedin
