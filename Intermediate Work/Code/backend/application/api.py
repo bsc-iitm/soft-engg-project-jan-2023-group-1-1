18cbbbcb4d36c9402e52c9fe787af05e3b392d41
venky96 <21f1000743@ds.study.iitm.ac.in>
2023-03-08 16:19:42 +0530

    GET, POST, DELETE Endpoints for Ticket
    Co-Authored-By: Chirag-Goel-17 <123762570+Chirag-Goel-17@users.noreply.github.com>

--- Intermediate Work/Code/backend/application/api.py
+++ Intermediate Work/Code/backend/application/api.py
@@ -6,13 +6,45 @@ from application.models import User, Student, Admin, Manager, Response, Ticket
 from application.models import token_required, db
 
 class TicketAPI(Resource):
-    def get(self):
-        # TO-DO: Chirag
-        pass
-    
-    def post(self):
-        #TO-DO: Chirag
-        pass
+    @token_required
+    def get(user,self):
+        if(user.role_id==1):
+            ticket=Ticket.query.filter_by(creator_id=user.user_id).all()
+            result=[]
+            for t in ticket:
+                d={}
+                d['ticket_id']=t.ticket_id
+                d['title']=t.title
+                d['description']=t.description
+                d['creation_date']=str(t.creation_date)
+                d['creator_id']=t.creator_id
+                d['number_of_upvotes']=t.number_of_upvotes
+                d['is_read']=t.is_read
+                d['is_open']=t.is_open
+                d['is_offensive']=t.is_offensive
+                d['is_FAQ']=t.is_FAQ
+                result.append(d)
+            return jsonify(result)
+        else:
+            abort(403,message="You are not authorized to view this page")
+    @token_required
+    def post(user,self):
+        if(user.role_id==1):
+            data=request.get_json()
+            ticket=Ticket(title=data['title'],
+                          description=data['description'],
+                          creation_date=datetime.now(),
+                          creator_id=user.user_id,
+                          number_of_upvotes=data['number_of_upvotes'],
+                          is_read=data['is_read'],
+                          is_open=data['is_open'],
+                          is_offensive=data['is_offensive'],
+                          is_FAQ=data['is_FAQ'])
+            db.session.add(ticket)
+            db.session.commit()
+            return jsonify({'message':'Ticket created successfully'})
+        else:
+            abort(403,message="You are not authorized to view this page")
         
     @token_required
     def patch(user, self):
@@ -83,10 +115,21 @@ class TicketAPI(Resource):
         else:
             abort(403,message= "You are not authorized to access this!")
         
-
-
-        
-
-    def delete(self):
-        #TO-DO: Varun
-        pass
+    @token_required
+    def delete(user, self):
+        try:
+            ticket_id = int(request.get_json()['ticket_id'])
+        except:
+            abort(400, 'ticket_id must exist and should be integer')
+        current_ticket = db.session.query(Ticket).filter(Ticket.ticket_id==ticket_id,Ticket.creator_id==user.user_id).first()
+        if current_ticket:
+            responses = db.session.query(Response).filter(Response.ticket_id==ticket_id).all()
+            if responses:
+                for post in responses:
+                    db.session.delete(post)
+                    db.session.commit() 
+            db.session.delete(current_ticket)
+            db.session.commit()
+            return 200, "OK"
+        else:
+            abort(400, 'No such ticket_id exists for the user')
