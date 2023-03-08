bb7aa6d7a45bd4502d66867d888f00a8da72338e
Chirag-Goel-17 <21f2000540@ds.study.iitm.ac.in>
2023-03-08 10:15:28 +0530

    get and post request of tickets

--- Intermediate Work/Code/backend/application/api.py
+++ Intermediate Work/Code/backend/application/api.py
@@ -1,17 +1,48 @@
-from flask_restful import Resource, request
+from flask_restful import Resource, request,abort
 from flask import jsonify
 from datetime import datetime
 from dateutil import tz, parser
-from application.models import User, Student, Admin, Manager, Response, Ticket
-
+from application.models import User, Student, Admin, Manager, Response, Ticket,db,token_required
 class TicketAPI(Resource):
-    def get(self):
-        # TO-DO: Chirag
-        pass
-
-    def post(self):
-        #TO-DO: Chirag
-        pass
+    @token_required
+    def get(user,self):
+        if(user.role_id==1):
+            ticket=Ticket.query.filter_by(creator_id=user.user_id).all()
+            result=[]
+            for t in ticket:
+                d={}
+                d['ticket_id']=t.ticket_id
+                d['title']=t.title
+                d['description']=t.description
+                d['creation_date']=str(t.creation_date)
+                d['creator_id']=t.creator_id
+                d['number_of_upvotes']=t.number_of_upvotes
+                d['is_read']=t.is_read
+                d['is_open']=t.is_open
+                d['is_offensive']=t.is_offensive
+                d['is_FAQ']=t.is_FAQ
+                result.append(d)
+            return jsonify(result)
+        else:
+            abort(403,message="You are not authorized to view this page")
+    @token_required
+    def post(user,self):
+        if(user.role_id==1):
+            data=request.get_json()
+            ticket=Ticket(title=data['title'],
+                          description=data['description'],
+                          creation_date=datetime.now(),
+                          creator_id=user.user_id,
+                          number_of_upvotes=data['number_of_upvotes'],
+                          is_read=data['is_read'],
+                          is_open=data['is_open'],
+                          is_offensive=data['is_offensive'],
+                          is_FAQ=data['is_FAQ'])
+            db.session.add(ticket)
+            db.session.commit()
+            return jsonify({'message':'Ticket created successfully'})
+        else:
+            abort(403,message="You are not authorized to view this page")
 
     def put(self):
         #TO-DO: Arya
