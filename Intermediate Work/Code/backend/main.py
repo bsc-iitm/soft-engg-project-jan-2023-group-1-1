6ac69c8617e62a51cd15deb17fbc81b219e0c0e4
Arya Bhattacharyya <21f2000436@student.onlinedegree.iitm.ac.in>
2023-03-08 22:21:16 +0530

    Added API for Response Class
    Response Class CRUD API with some additional option for responses with userID or responseID

--- Intermediate Work/Code/backend/main.py
+++ Intermediate Work/Code/backend/main.py
@@ -1,7 +1,10 @@
 from application import app, api
 
-from application.api import TicketAPI
+from application.api import TicketAPI, ResponseAPI_by_ticket, ResponseAPI_by_response_id, ResponseAPI_by_user
 api.add_resource(TicketAPI, '/api/ticket')
+api.add_resource(ResponseAPI_by_ticket, '/api/respTicket') #For getting responses with ticket_id
+api.add_resource(ResponseAPI_by_response_id, '/api/respResp') #For getting responses with response_id
+api.add_resource(ResponseAPI_by_user, '/api/respUser') #For getting responses with user id.
 
 from application.routes import *
 if __name__ == '__main__':
