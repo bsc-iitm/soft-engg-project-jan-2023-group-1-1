4ee50ce2080976ed5eb766be7816a3c3d7797e69
Varun <21f1000743@ds.study.iitm.ac.in>
2023-03-10 23:38:33 +0530

    Monthly Report for agent resolution time performace

--- Intermediate Work/Code/backend/application/tasks.py
+++ Intermediate Work/Code/backend/application/tasks.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from application.workers import celery
 from flask import current_app as app
 from application.models import db, User, Ticket, Response, Support_Agent
@@ -8,7 +9,51 @@ from celery.schedules import crontab
 @celery.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(hour=11, minute=30), unanswered_ticket_notification.s(), name='Daily Unanswered Ticket Reminder Reminder')
+    sender.add_periodic_task(crontab(minute=30, hour=4, day=1), poor_resolution_time.s(), name='Monthly Agent Resolution Time Report')
 
+
+@celery.task()
+def poor_resolution_time():
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+    agents = db.session.query(User).filter(User.role_id==2).all()
+    all_agents_avg_resolution_time = []
+    subject = 'This months agent resolution time report'
+    eid = db.session.query(User).filter(User.role_id==4).first().email_id
+
+    for agent in agents:
+        tickets_responded_by_agent = set(t.ticket_id for t in db.session.query(Response).filter(Response.responder_id == agent.user_id).all())
+        ticket_counter = 0
+        total_resolution_time = 0
+        open_tickets = 0
+        for ticket in tickets_responded_by_agent:
+            subqry = db.session.query(func.max(Response.response_id)).filter(Response.responder_id == agent.user_id, Response.ticket_id==ticket)
+            qry = db.session.query(Response).filter(Response.responder_id == agent.user_id, Response.ticket_id==ticket, Response.response_id == subqry).first()
+            tk = db.session.query(Ticket).filter(Ticket.ticket_id==ticket).first()
+            if tk.creation_date > thirty_days_ago :
+                if not tk.is_open:
+                    created_at = tk.creation_date
+                    resolved_at = qry.response_timestamp
+                    total_resolution_time += (resolved_at - created_at).total_seconds()/3600
+                    ticket_counter += 1
+                else:
+                    open_tickets +=1
+        if total_resolution_time > 0 and ticket_counter > 0:
+            avg_resolution_time = total_resolution_time/ticket_counter
+            if avg_resolution_time > 48:
+                all_agents_avg_resolution_time.append((agent.user_name, avg_resolution_time, open_tickets))  
+    if all_agents_avg_resolution_time:
+        html = '''
+            <html> 
+            <head> The following agents have a poor resolution time </head>
+            <body>
+            <ol>
+        '''
+        for tup in all_agents_avg_resolution_time:
+            html += f'<li> {tup[0]} has an average resolution time of {tup[1]} hours and has {tup[2]} unresolved tickets'
+        html+= '</ol> </body> </html>'
+        send_email.s((html, eid, subject)).apply_async()
+
+    return "OK"
 
 @celery.task()
 def unanswered_ticket_notification():
