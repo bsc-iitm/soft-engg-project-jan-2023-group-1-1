64c337729b92b02459a8045603e40404ca15ca4c
Chirag-Goel-17 <21f2000540@ds.study.iitm.ac.in>
2023-03-18 19:02:57 +0530

    login page frontend

---
+++ Intermediate Work/Code/frontend/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-light" style="width: 100%;">
+        <div class="container-fluid">
+            <router-link to="/DraggerComp" class="navbar-brand">APPLICATION</router-link>
+
+            <form class="d-flex" role="search">
+                <button class="btn btn-outline-danger" type="submit" @click="logout">Logout</button>
+            </form>
+        </div>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
