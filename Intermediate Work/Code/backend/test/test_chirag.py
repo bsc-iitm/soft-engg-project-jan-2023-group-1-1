9018845f3d42149bab78aed31f66097e9f2e39ad
Varun <21f1000743@ds.study.iitm.ac.in>
2023-03-20 18:26:09 +0530

    pytest Folder Structure
    Co-Authored-By: Arya Bhattacharyya <21f2000436@student.onlinedegree.iitm.ac.in>
    Co-Authored-By: Chirag-Goel-17 <123762570+Chirag-Goel-17@users.noreply.github.com>

---
+++ Intermediate Work/Code/backend/test/test_chirag.py
@@ -0,0 +1,4 @@
+# Ticket, User, ImportResourceUser Celery Task
+# GET: Check Status Code and Key-Value Pairs
+# POST/PATCH: Check Status Code, GET Request and Check Key-Value Pairs
+# DELETE: Delete Request, Get Status Code, GET Request and raise Error/not 200 status code
