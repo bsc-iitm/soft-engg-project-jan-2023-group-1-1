ef42ad1a48f6cfd3ac38d48a3a571e85c49d86af
Arya Bhattacharyya <21f2000436@student.onlinedegree.iitm.ac.in>
2023-03-25 18:29:21 +0530

    Update test_arya.py

--- Intermediate Work/Code/backend/test/test_arya.py
+++ Intermediate Work/Code/backend/test/test_arya.py
@@ -12,3 +12,25 @@ import os
 SCRIPT_DIRP = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIRP))
 
+from application.models import User,Ticket
+BASE="http://127.0.0.1:5000"
+url_ticket_all=BASE+"/api/ticketAll"
+url_getResolutionTimes=BASE+"/api/getResolutionTimes"
+
+def token_login_student():
+    url=BASE+"/login"
+    data={"email":"redding.abba@dollstore.org","password":"arya"}
+    response=requests.post(url,data=data)
+    return response.json()["token"]
+
+def token_login_support_agent():
+    url=BASE+"/login"
+    data={"email":"chirag@chirag.com","password":"chirag"}
+    response=requests.post(url,data=data)
+    return response.json()["token"]
+
+def token_login_admin():
+    url=BASE+"/login"
+    data={"email":"varun@varun.com","password":"varun"}
+    response=requests.post(url,data=data)
+    return response.json()["token"]
