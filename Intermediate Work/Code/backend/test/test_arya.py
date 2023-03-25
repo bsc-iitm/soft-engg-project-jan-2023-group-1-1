49041de9fd288709997540eee4ce76ee790e09fb
Arya Bhattacharyya <21f2000436@student.onlinedegree.iitm.ac.in>
2023-03-25 18:25:23 +0530

    Update test_arya.py

--- Intermediate Work/Code/backend/test/test_arya.py
+++ Intermediate Work/Code/backend/test/test_arya.py
@@ -2,3 +2,13 @@
 # GET: Check Status Code and Key-Value Pairs
 # POST/PATCH: Check Status Code, GET Request and Check Key-Value Pairs
 # DELETE: Delete Request, Get Status Code, GET Request and raise Error/not 200 status code
+
+import requests
+from flask import json
+#import db models here
+import sys
+import os
+
+SCRIPT_DIRP = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIRP))
+
