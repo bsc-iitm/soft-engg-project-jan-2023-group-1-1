c0ebf84aa04a302006dfc4649a9c4b7712b959ec
Arya Bhattacharyya <21f2000436@student.onlinedegree.iitm.ac.in>
2023-03-25 19:59:45 +0530

    Update test_arya.py

--- Intermediate Work/Code/backend/test/test_arya.py
+++ Intermediate Work/Code/backend/test/test_arya.py
@@ -1,4 +1,4 @@
-## TicketAll, GetResolutionTimes, ResolutionTimes, FlaggPostAPI
+## TicketAll, GetResolutionTimes, ResolutionTimes, FlaggPostAPI, ResponseAPI_by_ticket, getResponseAPI_by_ticket
 # GET: Check Status Code and Key-Value Pairs
 # POST/PATCH: Check Status Code, GET Request and Check Key-Value Pairs
 # DELETE: Delete Request, Get Status Code, GET Request and raise Error/not 200 status code
@@ -8,11 +8,12 @@ from flask import json
 #import db models here
 import sys
 import os
+from datetime import datetime
 
 SCRIPT_DIRP = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIRP))
 
-from application.models import Ticket
+from application.models import Ticket, Response
 BASE="http://127.0.0.1:5000"
 url_ticket_all=BASE+"/api/ticketAll"
 url_getResolutionTimes=BASE+"/api/getResolutionTimes"
@@ -33,6 +34,12 @@ def token_login_admin():
     url=BASE+"/login"
     data={"email":"varun@varun.com","password":"varun"}
     response=requests.post(url,data=data)
+    return response.json()["token"]
+
+def token_login_manager():
+    url = BASE+"/login"
+    data = {"email": "boss@boss.com", "password": "boss"}
+    response = requests.post(url, data = data)
     return response.json()["token"]
 
 #TICKET ALL GET Request.
@@ -100,3 +107,89 @@ def test_ticket_all_unauthenticated_patch():
     response=request.json()
     assert request.status_code==200
     assert response['status']=='unsuccessful, missing the authtoken'
+
+#GETRESOLUTIONTIMES POST REQUEST
+
+def test_getResolutionTimes_post_unauthenticated():
+    request=requests.post(url_getResolutionTimes)
+    response=request.json()
+    assert request.status_code==200
+    assert response['status']=='unsuccessful, missing the authtoken'
+
+def test_getResolutionTimes_post_wrong_role():
+    header={"secret_authtoken":token_login_admin(), "Content-Type":"application/json"}
+    input_dict = { "number_of_upvotes": 10023,"is_read": False, "ticket_id": 1e4}
+    data = json.dumps(input_dict)
+    request=requests.post(url = url_getResolutionTimes,data = data, headers=header)
+    response = request.json()
+    assert request.status_code == 404
+    assert response["message"] == "You are not authorized to access this feature!"
+
+def test_getResolutionTimes_post_no_ticket_id():
+    header={"secret_authtoken":token_login_manager(), "Content-Type":"application/json"}
+    input_dict = {}
+    data = json.dumps(input_dict)
+    request=requests.post(url = url_getResolutionTimes,data = data, headers=header)
+    response = request.json()
+    assert request.status_code == 403
+    assert response["message"] == "Please enter the ticket ID."
+    
+def test_getResolutionTimes_post_ticket_isopen():
+    header={"secret_authtoken":token_login_manager(), "Content-Type":"application/json"}
+    input_dict = {"ticket_id": 1}
+    data = json.dumps(input_dict)
+    request=requests.post(url = url_getResolutionTimes,data = data, headers=header)
+    response = request.json()
+    assert request.status_code == 404
+    assert response["message"] == "This ticket hasn't been responded to yet or is still open!"
+
+def test_getResolutionTimes_post_wrong_ticket_id():
+    header={"secret_authtoken":token_login_manager(), "Content-Type":"application/json"}
+    input_dict = {"ticket_id": 1000}
+    data = json.dumps(input_dict)
+    request=requests.post(url = url_getResolutionTimes,data = data, headers=header)
+    response = request.json()
+    assert request.status_code == 404
+    assert response["message"] == "No such ticket exists by the given ticket ID."
+
+def test_getResolutionTimes_post():
+    #Only checks if days, seconds, microseconds and ticket IDs match
+    header={"secret_authtoken":token_login_manager(), "Content-Type":"application/json"}
+    input_dict = {"ticket_id": 2}
+    data = json.dumps(input_dict)
+    request=requests.post(url = url_getResolutionTimes,data = data, headers=header)
+    response = request.json()
+    assert request.status_code == 200
+    responses = Response.query.filter_by(ticket_id = input_dict["ticket_id"]).all()
+    responses = list(responses)
+    ticket = Ticket.query.filter_by(ticket_id = input_dict["ticket_id"]).first()
+    a = {}
+    response_times = []
+    for thing in responses:
+        if isinstance(thing.response_timestamp, datetime):
+            #print("Here 1")
+            response_times.append(thing.response_timestamp)
+        elif isinstance(thing.response_timestamp, str):
+            #print("Here 2")
+            response_times.append(datetime.strptime(thing.response_timestamp,'%Y-%m-%d %H:%M:%S.%f'))
+        response_time = max(response_times)
+        a["creation_time"] = None
+        if isinstance(ticket.creation_date, str):
+            a["creation_time"] = datetime.strptime(ticket.creation_date, '%Y-%m-%d %H:%M:%S.%f')
+        elif isinstance(ticket.creation_date, datetime):
+            a["creation_time"] = ticket.creation_date
+        a["response_time"] = response_time
+        a["resolution_time_datetime_format"] = a["response_time"] - a["creation_time"]
+        a["days"] = a["resolution_time_datetime_format"].days
+        a["seconds"] = a["resolution_time_datetime_format"].seconds
+        a["microseconds"] = a["resolution_time_datetime_format"].microseconds
+        a["resolution_time_datetime_format"] = str(a["resolution_time_datetime_format"])
+        a["creation_time"] = a["creation_time"]
+        a["ticket_id"] = input_dict["ticket_id"]
+        a["response_time"] = None
+        a["resolution_time_datetime_format"] = None
+        a["creation_time"] = None
+    d = response["data"]
+    for keys in a:
+        if a[keys] is not None:
+            assert a[keys] == d[keys]
