eef621b16bed2255b72844916e0b77e8249e9feb
Arya Bhattacharyya <21f2000436@student.onlinedegree.iitm.ac.in>
2023-03-25 18:54:21 +0530

    Update test_arya.py

--- Intermediate Work/Code/backend/test/test_arya.py
+++ Intermediate Work/Code/backend/test/test_arya.py
@@ -12,7 +12,7 @@ import os
 SCRIPT_DIRP = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIRP))
 
-from application.models import User,Ticket
+from application.models import Ticket
 BASE="http://127.0.0.1:5000"
 url_ticket_all=BASE+"/api/ticketAll"
 url_getResolutionTimes=BASE+"/api/getResolutionTimes"
@@ -34,3 +34,26 @@ def token_login_admin():
     data={"email":"varun@varun.com","password":"varun"}
     response=requests.post(url,data=data)
     return response.json()["token"]
+
+def test_ticket_all_get():
+    header = {"secret_authtoken":token_login_student()}
+    request=requests.get(url_ticket_all,headers=header)
+    tickets = list(Ticket.query.filter_by().all())
+    response = request.json()
+    responses = response["data"]
+    assert request.status_code==200
+    for d in responses:
+        for q in tickets:
+            if q.ticket_id == d['ticket_id']:
+                assert d['ticket_id'] ==  q.ticket_id
+                assert d['title']==q.title
+                assert d['description']==q.description
+                assert d['creation_date']== str(q.creation_date)
+                assert d['creator_id']==q.creator_id
+                assert d['number_of_upvotes']==q.number_of_upvotes
+                assert d['is_read']==q.is_read
+                assert d['is_open']==q.is_open
+                assert d['is_offensive']== q.is_offensive
+                assert d['is_FAQ']==q.is_FAQ
+                assert d['rating']==q.rating
+
