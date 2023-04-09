db5fc9955c7011a1d967b9b9a48c0a24673ae462
Varun <21f1000743@ds.study.iitm.ac.in>
2023-04-09 20:50:33 +0530

    Ticket Open/Close status change upon response

--- Intermediate Work/Code/frontend/src/components/ResponseComponent.vue
+++ Intermediate Work/Code/frontend/src/components/ResponseComponent.vue
@@ -11,7 +11,7 @@
                 <p class="response">{{ r.response }}</p>
                 <hr />
             </div>
-            <form v-on:submit="addResponse">
+            <form v-on:submit.prevent="addResponse">
                 <div class="row">
                     <div class="col-md-10">
                         <input type="text" class="form-control" v-model="response" placeholder="Enter Response" />
@@ -26,26 +26,49 @@
 </template>
 <script>
 import axios from 'axios';
+import { mapGetters } from "vuex";
 
 export default {
     name: "ResponseComponent",
+    computed: {
+        ...mapGetters(["role", "user_id"]),
+    },
     data() {
         return {
             title: "",
             description: "",
             is_read: 0,
+            is_open: null,
             ticket_id: this.$route.params.ticketId,
+            cid : null,
             response: "",
             responses: []
         };
     },
     methods: {
+        async editOpen() {
+            // If ticket is marked as close and student replies then ticket gets reopened
+            if (this.$store.state.role==1 && !this.is_open && this.$store.state.user_id==this.cid) {
+                let response = await axios.patch('/api/ticket', {
+                    ticket_id: this.ticket_id,
+                    is_open: true
+                });
+                console.log(response);
+            } else if (this.$store.state.role==2 && this.is_open) {
+                let response = await axios.patch('/api/ticketAll', {
+                    ticket_id: this.ticket_id,
+                    is_open: false
+                });
+                console.log(response);
+            }
+        },
         async addResponse() {
             // console.log(this.response)
             await axios.post("/api/respTicket", {
                 ticket_id: this.ticket_id,
                 response: this.response
             }).then((res) => {
+                this.editOpen();
                 this.response = "";
                 this.$router.go();
                 console.log(res);
@@ -62,6 +85,8 @@ export default {
                     this.title = res.data.data[i].title;
                     this.description = res.data.data[i].description;
                     this.is_read = res.data.data[i].is_read;
+                    this.is_open = res.data.data[i].is_open;
+                    this.cid = res.data.data[i].creator_id;
                 }
             }
         });
