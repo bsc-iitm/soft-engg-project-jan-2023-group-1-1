14f493dab3495ec634ca6e181e78422a3ab1468b
Chirag-Goel-17 <21f2000540@ds.study.iitm.ac.in>
2023-04-13 10:42:53 +0530

    Admin frontend corrections

--- Intermediate Work/Code/frontend/src/components/ManageFAQComponent.vue
+++ Intermediate Work/Code/frontend/src/components/ManageFAQComponent.vue
@@ -2,10 +2,9 @@
     <div class="container">
         <div class="topic-container">
             <h3>The following tickets have been suggested for FAQ </h3>
-            <br/>
-            <hr/>
-            <br/>
-            <div class="container" v-for="(t, index) in relevant_tickets"  :key="index">
+            <br />
+            <hr />
+            <div class="container" v-for="(t, index) in relevant_tickets" :key="index">
                 <div class="row">
                     <div class="col-md-10">
                         <RouterLink :to="{ name: 'response', params: { ticketId: t.ticket_id } }">
@@ -14,45 +13,48 @@
                             </p>
                         </RouterLink>
                         <p>{{ t.description }}</p>
-                        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#faqModal" @click="this.selected_ticket=t.ticket_id"> Approve </button>
                     </div>
                     <div class="col-md-2">
-                        <div class="row">
-                            <button class="btn upvote disabled">^<br>{{t.number_of_upvotes }}</button>
-                        </div>
+                        <button class="btn upvote disabled">^<br>{{ t.number_of_upvotes }}</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <button class="btn btn-success rounded" data-bs-toggle="modal" data-bs-target="#faqModal" @click="this.selected_ticket=t.ticket_id"> Approve </button>
+                    </div>
+                    <div class="col-md-8">
 
-                        <div class="row">
-                            <button class="btn btn-danger" @click="rejectFAQ(t.ticket_id)"> Reject </button>
-
-                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-danger rounded" @click="rejectFAQ(t.ticket_id)"> Reject </button>
                     </div>
                 </div>
             </div>
-            <br/>
-        <hr/>
+            <hr />
         </div>
-        <br/>
+        <br />
         <!-- Modal -->
-<div class="modal fade" id="faqModal" tabindex="-1" aria-labelledby="faqModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="faqModalLabel">Please choose the appropriate category</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <select v-model="selected">
-            <option disabled value="">Please select one</option>
-            <option v-for="(c, index) in categories"  :key="index">{{ c }}</option>
-        </select>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success" data-bs-dismiss="modal" @click="approveFAQ()">Submit</button>
-      </div>
-    </div>
-  </div>
-</div>
+        <div class="modal fade" id="faqModal" tabindex="-1" aria-labelledby="faqModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="faqModalLabel">Please choose the appropriate category</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <select v-model="selected">
+                            <option disabled value="">Please select one</option>
+                            <option v-for="(c, index) in categories" :key="index">{{ c }}</option>
+                        </select>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-success" data-bs-dismiss="modal"
+                            @click="approveFAQ()">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
@@ -64,7 +66,7 @@ import axios from 'axios';
 export default {
     name: "ManageFAQSuggestionsComponent",
     methods: {
-        async approveFAQ(){
+        async approveFAQ() {
             var res = await axios.post('/api/faq', {
                 ticket_id: this.selected_ticket,
                 is_approved: true,
@@ -91,19 +93,19 @@ export default {
             relevant_tickets: [],
             categories: []
         };
-    
+
     },
     async created() {
         var res3 = await axios.get('/api/category');
         this.categories = res3.data.data;
         var res = await axios.get('/api/faq');
         this.faqs = res.data.data;
-        this.faq_ticket_ids = this.faqs.map(({ticket_id}) => ticket_id)
+        this.faq_ticket_ids = this.faqs.map(({ ticket_id }) => ticket_id)
         var res2 = await axios.get('/api/ticketAll');
         for (var i = 0; i < res2.data.data.length; i++) {
             if (res2.data.data[i].is_FAQ) {
                 // eslint-disable-next-line
-                if (this.faq_ticket_ids.includes(res2.data.data[i].ticket_id)) {} else {
+                if (this.faq_ticket_ids.includes(res2.data.data[i].ticket_id)) { } else {
                     this.relevant_tickets.push(res2.data.data[i]);
                 }
             }
--- Intermediate Work/Code/frontend/src/components/ManageUsersComponent.vue
+++ Intermediate Work/Code/frontend/src/components/ManageUsersComponent.vue
@@ -1,52 +1,70 @@
 <template>
     <div class="container">
-        <div class="topic-container">
-            <h3>ADD A USER BY EMAIL ID</h3>
-        </div>
-        <br />
-        <form v-on:submit="addUser">
-            <!-- <div class="row"> -->
-                <!-- <div class="col-md-10"> -->
-                    <div class="form-group"><label>Enter Email ID</label><input type="text" class="form-control" v-model="emailID"  required /></div>
-                <!-- </div> -->
-                <!-- <br />
-                <br />
-                <br />
-                <div class="col-md-10">
-                    <input type="text" class="form-control" v-model="roleID" placeholder="Enter Role" required />
-                </div> -->
-                <br/>
-                
-                <div class="form-group"><label>Choose Role</label><select v-model="roleID" class="form-select">
-                    <option value="student">Student</option>
-                    <option value="support agent">Support Agent</option>
-                    <option value="admin">Admin</option>
-                    <option value="manager">Manager</option>
-                    
-                </select></div>
+        <div class="container" style="margin-top: 20px;">
+            <p>
+                <button class="btn btn-primary heading" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    ADD A USER BY EMAIL ID
+                </button>
+            </p>
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    <form v-on:submit="addUser">
+                        <div class="form-group">
+                            <label>Enter Email ID</label>
+                            <input type="text" class="form-control" v-model="emailID" required />
+                        </div>
+                        <br />
+                        <div class="form-group"><label>Choose Role</label><select v-model="roleID" class="form-select">
+                                <option value="student">Student</option>
+                                <option value="support agent">Support Agent</option>
+                                <option value="admin">Admin</option>
+                                <option value="manager">Manager</option>
 
-                <!-- <div class="col-md-2"> -->
-                    
-                    <button class="btn btn-lg" type="submit">Submit</button>
-                <!-- </div> -->
-            <!-- </div> -->
-        </form>
-        <hr/>
-        <div class="topic-container">
-            <h3>ADD MULTIPLE USERS BY CSV FILE</h3>
+                            </select>
+                        </div>
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <br/>
-        <label> Please upload a CSV file if you wish to import data</label><br/>
-        <div class="form-group"><input class="btn btn-lg" ref="file" id = file type="file" accept=".csv" v-on:change="onUpload($event)" /></div>
-        
-        <button class="btn btn-outline-success rounded" @click="fileSubmission">Submit</button>
-        <hr/>
-
-        <div class = "topic-container">
-            <h3>DELETE USER BY USERNAME</h3>
+        <hr />
+        <div class="container">
+            <p>
+                <button class="btn btn-primary heading" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseExample2" aria-expanded="false" aria-controls="collapseExample">
+                    ADD MULTIPLE USERS BY CSV FILE
+                </button>
+            </p>
+            <div class="collapse" id="collapseExample2">
+                <div class="card card-body">
+                    <label> Please upload a CSV file if you wish to import data</label><br />
+                    <div class="form-group"><input class="btn btn-lg" ref="file" id=file type="file" accept=".csv"
+                            v-on:change="onUpload($event)" /></div>
+                    <div class="col-md-4">
+                        <button class="btn btn-primary" @click="fileSubmission">Submit</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class = "form-group"><label>Enter the username you wish to delete</label><input type="text" class="form-control" v-model="username"  required /></div>
-        <button class="btn btn-lg" type="submit" @click="onDelete">Delete</button>
+        <hr />
+        <div class="container">
+            <p>
+                <button class="btn btn-primary heading" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseExample3" aria-expanded="false" aria-controls="collapseExample">
+                    DELETE USER BY USERNAME
+                </button>
+            </p>
+            <div class="collapse" id="collapseExample3">
+                <div class="card card-body">
+                    <div class="form-group"><label>Enter the username you wish to delete</label><input type="text"
+                            class="form-control" v-model="username" required /></div>
+                    <div class="col-md-4">
+                        <button class="btn btn-primary" type="submit" @click="onDelete">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -66,23 +84,23 @@ export default {
 
     },
     methods: {
-        async onDelete(x){
+        async onDelete(x) {
             x.preventDefault();
             await axios.get("/api/user").then((res) => {
-                    let data = res.data.data;
-                    this.usernames = data.map(({user_name}) => user_name)
-                    if (!this.usernames.includes(this.username)){
-                        alert("This username doesn't exist or is an admin/manager.");
-                        this.$router.go();
-                    }
-                    else{
-                        data.forEach(x => {if (x.user_name == this.username){this.user_id = x.user_id; console.log(this.user_id)}});
-                    }
-                    
-                }).catch((err) => {
-                    console.log(err);
-                });
-            await axios.delete(`/api/user/${this.user_id}`).then(() =>{alert("User deleted successfully");this.$router.go();}).catch((err) =>{console.log(err);})
+                let data = res.data.data;
+                this.usernames = data.map(({ user_name }) => user_name)
+                if (!this.usernames.includes(this.username)) {
+                    alert("This username doesn't exist or is an admin/manager.");
+                    this.$router.go();
+                }
+                else {
+                    data.forEach(x => { if (x.user_name == this.username) { this.user_id = x.user_id; console.log(this.user_id) } });
+                }
+
+            }).catch((err) => {
+                console.log(err);
+            });
+            await axios.delete(`/api/user/${this.user_id}`).then(() => { alert("User deleted successfully"); this.$router.go(); }).catch((err) => { console.log(err); })
         },
         async addUser(x) {
             // console.log(this.response)
@@ -125,30 +143,30 @@ export default {
                 });
             }
         },
-        onUpload(){
+        onUpload() {
             this.myFile = this.$refs.file.files[0];
-            
+
         },
-        fileSubmission(){
+        fileSubmission() {
             let formData = new FormData();
             console.log(this.myFile);
-            formData.append('file',this.myFile);
-            
+            formData.append('file', this.myFile);
+
             let authtoken = localStorage.getItem("token");
             fetch("http://localhost:5000/api/importUsers", {
-            method: 'POST', 
-            mode: 'cors', 
-            cache: 'no-cache', 
-            credentials: 'omit', 
-            headers: {
-              'secret_authtoken' : authtoken,
-              //'Content-Type': "multipart/form-data",
-            },
-            redirect: 'follow', 
-            referrerPolicy: 'no-referrer', 
-            body: formData 
-          }).then ( () => {alert("User addition request has been received. You will get notified via email on the status.");/*location.reload()*/}).catch( e => console.log(e));
-    },
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'omit',
+                headers: {
+                    'secret_authtoken': authtoken,
+                    //'Content-Type': "multipart/form-data",
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: formData
+            }).then(() => { alert("User addition request has been received. You will get notified via email on the status.");/*location.reload()*/ }).catch(e => console.log(e));
+        },
     }
 }
 </script>
@@ -156,7 +174,20 @@ export default {
 .topic-container {
     margin: 33px 63px;
 }
+
 .form-group {
     margin-bottom: 1.5rem;
+}
+
+.heading {
+    background-color: white;
+    color: black;
+    font-size: 25px;
+}
+
+.heading:hover {
+    background-color: #f1f1f1;
+    color: black;
+    font-size: 20px;
 }
 </style>
